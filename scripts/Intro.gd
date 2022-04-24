73aec087b8bb984ea258431fcacc4267772f340a
Christian Wieprecht <15862318+cwiep@users.noreply.github.com>
2022-04-24 21:58:31 +0200

    Improve intro text

--- scripts/Intro.gd
+++ scripts/Intro.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
-const text = "'Welcome, Agent.\nWe will send you to steal some secret documents.\n\nYou will use the\nCarelessly\nHacked\nAction\nInjection\nNibbler\nor C.H.A.I.N. in short.'"
+var text = "'Welcome, Agent.\nWe will send you to steal some secret documents.\n\nYou will use the\nCarelessly\nHacked\nAction\nInjection\nNeuroaction-o-meter\nor C.H.A.I.N. in short.'"
+var text2 = "'With C.H.A.I.N. you are able to plan your chain of actions in a very detailed fashion as the following introduction shall explain.\n\nOh, and\nDON'T LOSE C.H.A.I.N.\n... we only have one!"
 var counter = 0
 var length = 0
+var next_text = false
 
 func _process(delta):
 	counter += delta
@@ -16,5 +18,9 @@ func _input(event):
 		if length < text.length():
 			length = text.length()
 			$Text.text = text
-		else:
+		elif !next_text:
+			next_text = true
+			text = text2
+			length = 0
+		elif next_text:
 			get_tree().change_scene("res://scenes/Tutorial.tscn")
